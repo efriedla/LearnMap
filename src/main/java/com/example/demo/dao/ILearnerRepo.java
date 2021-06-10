431963fee409faf2d90900931b12a439ab8e9924
moocow <efriedla20852@gmail.com>
2021-06-10 14:15:49 -0400

    crud

---
+++ src/main/java/com/example/demo/dao/ILearnerRepo.java
@@ -0,0 +1,16 @@
+package com.example.demo.dao;
+/*
+        - add annotation
+        - extend spring jpa
+        - add custom methods if needed
+ */
+
+import com.example.demo.models.Learner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ILearnerRepo extends JpaRepository<Learner, Long> {
+
+	void deleteLearnerById(Long id);
+
+	Learner findLearnerById(Long id);
+}
---
+++ src/main/java/com/example/demo/services/LearnerService.java
@@ -0,0 +1,51 @@
+package com.example.demo.services;
+ /*
+            - add class annotations
+            - add @Transactional on class or on each method
+            - add crud methods
+     */
+
+import com.example.demo.dao.ILearnerRepo;
+import com.example.demo.models.Learner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class LearnerService {
+	private final ILearnerRepo learnerRepo;
+
+	@Autowired
+	public LearnerService(ILearnerRepo learnerRepo) {
+		this.learnerRepo = learnerRepo;
+	}
+
+	public Learner addLearner(Learner learner){
+		learner.setFirstName("bob");
+		return learnerRepo.save(learner);
+	}
+
+	public List<Learner> findAllLearners(){
+		return learnerRepo.findAll();
+	}
+
+	public Learner updateLearner(Learner learner){
+		return  learnerRepo.save(learner);
+	}
+
+	public Learner findLearnerById(Long id){
+		//return learnerRepo.findLearnerById(id).orElseThrow(() -> new UserPrincipalNotFoundException("User by id " + id + " was not found"));
+
+
+		return learnerRepo.findLearnerById(id);
+	}
+
+	public void deleteLearner(Long id){
+		learnerRepo.deleteLearnerById(id);
+	}
+
+
+}
