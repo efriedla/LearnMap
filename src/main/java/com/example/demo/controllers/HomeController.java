634500f4b67c8e998abb5eb0aaf0f9b956c6d438
moocow <efriedla20852@gmail.com>
2021-06-16 09:19:01 -0400

    added more to resource

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,16 +1,31 @@
 package com.example.demo.controllers;
 
+
+import com.example.demo.services.LearnerService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
+
+
+	@GetMapping("/")
+	public String index() {
+		System.out.println("inside index action");
+		return "redirect:index";
+	}
+	@RequestMapping(value = "/index")
+	public String index2() {
+		System.out.println("inside index2 action");
+		return "index";
+	}
 
 	@GetMapping("/template")
 	public String template(){
-		System.out.println("inside index action");
+		System.out.println("inside template action");
 		return "template";
 	}
 }
--- src/main/java/com/example/demo/controllers/LearnerController.java
+++ src/main/java/com/example/demo/controllers/LearnerController.java
@@ -2,25 +2,41 @@ package com.example.demo.controllers;
 
 import com.example.demo.models.Learner;
 import com.example.demo.services.LearnerService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("learner")
+@Log
 public class LearnerController {
+	@Autowired
 	private final LearnerService learnerService;
 
 	public LearnerController(LearnerService learnerService) {
 		this.learnerService = learnerService;
 	}
 
+	// creates student object that can remeber what was entered for sessions
+	@ModelAttribute("learnerObject")
+	public String learnerObject() {
+		return "";
+	}
+
 	@GetMapping("/template")
-	public String template(){
+	public String test(){
 		System.out.println("inside index action");
 		return "template";
+	}
+	@GetMapping({"/","index"})
+	public String index(){
+		System.out.println("******** in LEarner Controller!!!");
+		return "index";
 	}
 
 	@GetMapping("/all")
@@ -28,6 +44,7 @@ public class LearnerController {
 		List<Learner> learners =  learnerService.findAllLearners();
 		return new ResponseEntity<>(learners, HttpStatus.OK);
 	}
+
 
 	@GetMapping("/find/{id}")
 	public ResponseEntity<Learner> getLearnerById (@PathVariable Long id){
