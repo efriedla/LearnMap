ca41a3a8d813ef8bf032a1ce6829c142fffec864
moocow <efriedla20852@gmail.com>
2021-06-20 18:33:55 -0400

    added security and validation to pom, + added auth model

---
+++ src/main/java/com/example/demo/dao/ICourseRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import com.example.demo.models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ICourseRepo extends JpaRepository<Course, Long> {
+}
